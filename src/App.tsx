bbd715e54a3890d480b79f0ed4d4350b2fca0f7f
Ruei <ruei@null.computer>
2023-02-27 21:31:06 +0800

    update: add animation for weather info

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { animated, useTransition } from '@react-spring/web'
 import { Stats } from '@react-three/drei';
 import Experience from './components/Experience';
 import WeatherInfo from './components/WeatherInfo';
@@ -12,10 +13,25 @@ type Weather = {
   wx?: string;
 };
 
+const AnimatedWeatherInfo = animated(WeatherInfo);
+
 export default function App() {
   const { data, isLoading, error } = useWeather();
   const [location, setLocation] = useState<string>('');
   const [weather, setWeather] = useState<Weather>();
+
+  const transition = useTransition(weather ?? [], {
+    from: {
+      opacity: 0,
+      y: 80,
+    },
+    enter: {
+      opacity: 1,
+      y: 0
+    },
+    exitBeforeEnter: true,
+
+  });
 
   useEffect(() => {
     if (isLoading || !data) {
@@ -34,10 +50,14 @@ export default function App() {
 
       <Experience setLocation={setLocation} />
 
-      <div className="px-12 py-8 mx-auto w-full max-w-[1200px] h-full dark:text-gray-50 relative pointer-events-none">
+      <div className="px-12 py-10 mx-auto w-full max-w-[1200px] h-full dark:text-gray-50 relative pointer-events-none">
         <h1 className="mb-6 py-4 text-4xl font-bold">台灣天氣預報</h1>
 
-        {weather && <WeatherInfo weatherElement={weather} />}
+        {weather && (
+          transition((style) => (
+            <AnimatedWeatherInfo weatherElement={weather} style={style} />
+          ))
+        )}
       </div>
     </>
   );
--- src/components/WeatherInfo/WeatherInfo.tsx
+++ src/components/WeatherInfo/WeatherInfo.tsx
@@ -1,3 +1,4 @@
+
 type WeatherElement = {
   location?: string;
   temp?: [string, string];
@@ -9,17 +10,18 @@ type Props = {
   weatherElement?: WeatherElement;
 };
 
-export default function WeatherInfo({ weatherElement }: Props ) {
+export default function WeatherInfo({ weatherElement, ...delegated }: Props ) {
   const temperatureRange = `${weatherElement?.temp?.[0]} ~ ${weatherElement?.temp?.[1]}`;
 
   return (
-    <div className="p-8 w-[440px] flex flex-col gap-4 dark:bg-neutral-900 rounded-lg shadow-lg backdrop-blur">
+    <div className="p-10 w-[440px] flex flex-col gap-4 dark:bg-neutral-900/80 rounded-lg shadow-lg backdrop-blur" {...delegated}>
       <div>
         <h2 className="text-3xl">{weatherElement?.location}</h2> 
         <p className="text-md italic text-gray-500">{weatherElement?.ci}</p>
       </div>
 
       <p className="text-4xl">{temperatureRange}</p>
-    </div> 
+      <p className="text-2xl">{weatherElement?.wx}</p>
+    </div>
   );
 }
