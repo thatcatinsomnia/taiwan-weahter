f99fd63a5ab1ebdc60ac16232a5b5dd870e93060
Ruei <ruei@null.computer>
2023-02-27 17:26:43 +0800

    fix: fix weather api to get correct index of time

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -16,7 +16,7 @@ type WeatherTime = {
   };
 };
 
-type ElementName = 'MinT' | 'MaxT' | 'Wx' | 'PoP' | 'CI';
+type ElementName = 'MinT' | 'MaxT' | 'Wx' | 'CI';
 
 type WeatherElement = {
   elementName: ElementName;
@@ -34,9 +34,16 @@ type FormatedWeather = {
   location?: string;
   temp?: [string, string];
   wx?: string;
+  ci?: string;
 };
 
+// Wx: 天氣現象
+// MaxT: 最高溫度
+// MaxT: 最低溫度
+// CI: 舒適度
 const fetchWeatherData = async () => {
+  const REQUIRED_ELEMENT_LIST = ['Wx', 'MaxT', 'MinT', 'CI'];
+
   const res = await axios.get(`${API_URL}?Authorization=${import.meta.env.VITE_WEATHER_AUTH_KEY}`);
   const data = await res.data;
 
@@ -44,28 +51,37 @@ const fetchWeatherData = async () => {
 
   return locations.map(location => {
     // fiter the elements we need.
-    const requiredWeatherElements = location.weatherElement.filter(el => el.elementName === 'Wx' || el.elementName === 'MaxT' || el.elementName === 'MinT');
+    const requiredWeatherElements = location.weatherElement.filter(el => REQUIRED_ELEMENT_LIST.includes(el.elementName));
 
     const now = new Date();
-    const timeIndexOfNow = requiredWeatherElements[0].time.findIndex(time => isAfter(now, new Date(time.startTime)) && isBefore(now, new Date(time.endTime)));
+    
+    const timeIndexOfNow = requiredWeatherElements[0].time.findIndex(time => {
+      const startTime = new Date(time.startTime);
+      const endTime = new Date(time.endTime);
 
+      return isAfter(now, startTime) && isBefore(now, endTime)
+    });
+    
     // formated weather data, we only need wx, maxT, minT,
     // store minT and maxT in array, so we can use it to display temperature range later.
     return requiredWeatherElements.reduce<FormatedWeather>((total, element) => {
       const elementName = element.elementName;
-      const elementTime = element.time[timeIndexOfNow];
+
+      // the api may not have the current time data, cause it weather "forecast", the time will be in the future, if so we set index to 0 to get first data
+      const elementTime = element.time[timeIndexOfNow] || element.time[0];
 
       // initlal total['temp'] and ['location'] to prevent error
       total['temp'] = total['temp'] ? total['temp'] : [] as unknown as [string, string];
       total['location'] = total['location'] ? total['location'] : location.locationName;
 
-      if (elementName === 'Wx') {
-        total['wx'] = elementTime.parameter.parameterName;
-      } else if (elementName === 'MinT') {
+      if (elementName === 'MinT') {
         total['temp'][0] = `${elementTime.parameter.parameterName} ℃`;
-      } else {
+      } else if (elementName === 'MaxT') {
         total['temp'][1] = `${elementTime.parameter.parameterName} ℃`;
-      }
+      } else {
+        const lowerCaseElementName = elementName.toLocaleLowerCase() as Lowercase<typeof elementName>;
+        total[lowerCaseElementName] = elementTime.parameter.parameterName;
+      }     
       
       return total;
     }, {} as FormatedWeather);
@@ -76,7 +92,8 @@ export default function useWeather() {
   const { data, error, isLoading, isError } = useQuery({
     queryKey: ['weathers'],
     queryFn: fetchWeatherData,
-    staleTime: Infinity
+    staleTime: Infinity,
+    retry: false
   });
 
   return {
